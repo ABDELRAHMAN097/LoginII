9a6034770df61b2cc20e68f68a0f76ce0f2583cc
boody <bodymagdy97@gmail.com>
2023-12-07 09:20:04 +0200

    x

--- maiin.js
+++ maiin.js
@@ -11,3 +11,45 @@ function switching(){
 
 }
 sitting.addEventListener("click",switching);
+// darMood
+let moon = document.querySelector(".fa-moon");
+let form = document.querySelector(".form");
+let jjj = document.querySelector(".jjj");
+
+function switchMood(){
+  form.classList.toggle("jjj");
+}
+moon.addEventListener("click",switchMood);
+// sssssssssssssss
+
+let lastButton =document.querySelector(".lastButton");
+let move1 =document.querySelector(".move1");
+let move2 =document.querySelector(".move2");
+let move3 =document.querySelector(".move3");
+let move4 =document.querySelector(".move4");
+let moving = [move1 , move2 , move3 , move4];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
